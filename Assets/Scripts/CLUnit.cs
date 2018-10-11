fac33647fe7f01bffa2a68bfb5ab2cf86302ec91
Minstreams <minstreams@qq.com>
2018-10-11 20:46:23 +0800

    鬼打墙地图编辑器制作中

--- Assets/Scripts/CLUnit.cs
+++ Assets/Scripts/CLUnit.cs
@@ -6,12 +6,27 @@ using UnityEngine;
 /// 地图物体单位
 /// </summary>
 [DisallowMultipleComponent]
-public class CLUnit : MonoBehaviour {
+public class CLUnit : MonoBehaviour
+{
     /// <summary>
-    /// 在Manager注册的角度
+    /// 在CLManager注册的角度，用于排序和动态加载
     /// </summary>
     public float angle;
+    /// <summary>
+    /// 在CLManager注册的index，用于网络同步,只需要在Group保存时，和生成最终地图时更新
+    /// </summary>
+    public int index;
+    /// <summary>
+    /// unit激活状态
+    /// </summary>
+    public bool Active { get; private set; }
 
+    public void SetActive(bool b)
+    {
+        //if (Active == b) return;
+        Active = b;
+        gameObject.SetActive(b);
+    }
 
     //TODO：
     //激活
@@ -19,4 +34,11 @@ public class CLUnit : MonoBehaviour {
     //依据恐慌值变形
 
 
+#if UNITY_EDITOR
+    private void OnDestroy()
+    {
+        CircleLoopManager.DeleteUnit(this);
+    }
+
+#endif
 }
